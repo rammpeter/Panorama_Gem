170d37a0e2ee48a54bf5623c84030dfff1b0b28a
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2016-10-27 15:18:32 +0200

    Not using asset-path because it doesn't respect jar-prefix

--- lib/panorama/version.rb
+++ lib/panorama/version.rb
@@ -1,6 +1,6 @@
 module Panorama
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.4.6'
+  VERSION = '2.4.7'
   RELEASE_DATE = Date.parse('2016-10-27')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
