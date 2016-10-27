ec69143907402574afb865d7a6fb0c16d6c2aee7
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2016-10-27 16:54:05 +0200

    Move helpers from module panorama for coverage

--- app/helpers/menu_extension_helper.rb
+++ app/helpers/menu_extension_helper.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 # Wird von MenuHelper inkludiert, kann in App Engine überschrieben werden
 
-module Panorama::MenuExtensionHelper
+module MenuExtensionHelper
   def extend_main_menu main_menu  # Methode kann in App fuer Erweiterung des Menüs überschrieben werden
     main_menu
   end
