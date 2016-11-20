392ccb1d32702da7f4e8b9271800b2358386dd00
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2016-11-20 13:00:42 +0100

    Fix "Session-ID"

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -1,7 +1,7 @@
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.4.46'
-  RELEASE_DATE = Date.parse('2016-11-14')
+  VERSION = '2.4.47'
+  RELEASE_DATE = Date.parse('2016-11-20')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
