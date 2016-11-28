1ed01887522b009744dfda1733044a75ce621fe0
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2016-11-28 18:09:43 +0100

    include turbolinks

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -1,6 +1,6 @@
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.4.50'
+  VERSION = '2.4.51'
   RELEASE_DATE = Date.parse('2016-11-28')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
