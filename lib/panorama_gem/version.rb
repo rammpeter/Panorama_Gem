adb34dc2df9e827090dc176a5139aaebca5c382a
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2016-12-13 14:42:55 +0100

    New menu entry feature usage

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -1,7 +1,7 @@
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.4.60'
-  RELEASE_DATE = Date.parse('2016-12-09')
+  VERSION = '2.4.61'
+  RELEASE_DATE = Date.parse('2016-12-13')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
