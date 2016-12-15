cd605c53eed3152971dadcd5cab542adf0918555
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2016-12-15 11:35:21 +0100

    Version

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -1,7 +1,7 @@
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.4.61'
-  RELEASE_DATE = Date.parse('2016-12-13')
+  VERSION = '2.4.62'
+  RELEASE_DATE = Date.parse('2016-12-15')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
