7d0c434471ca5857f06da8bab56a9aa1eaa3a945
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2016-12-23 14:40:59 +0100

    Switch to Rails 5.0.1

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -1,6 +1,6 @@
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.4.69'
+  VERSION = '2.4.70'
   RELEASE_DATE = Date.parse('2016-12-23')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
