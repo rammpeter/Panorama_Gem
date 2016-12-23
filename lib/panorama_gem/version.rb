4d4deaa593c71fe966a82e043782a38916b1ae6c
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2016-12-23 14:23:57 +0100

    Extended error logging

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -1,6 +1,6 @@
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.4.68'
+  VERSION = '2.4.69'
   RELEASE_DATE = Date.parse('2016-12-23')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
