9e64798ce507278d531400f5fab97dbd3116eda5
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2017-01-10 11:49:26 +0100

    Use data-type=html for ajax-calls, adjust tests

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -1,7 +1,7 @@
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.4.78'
-  RELEASE_DATE = Date.parse('2017-01-08')
+  VERSION = '2.5.0'
+  RELEASE_DATE = Date.parse('2017-01-10')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
