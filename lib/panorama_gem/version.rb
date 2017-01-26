5e5f8e33f7055540af3917f546b7fc1751d1717b
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2017-01-26 13:42:21 +0100

    Capybara tests switched to poltergeist

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -1,7 +1,7 @@
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.5.17'
-  RELEASE_DATE = Date.parse('2017-01-25')
+  VERSION = '2.5.18'
+  RELEASE_DATE = Date.parse('2017-01-26')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
