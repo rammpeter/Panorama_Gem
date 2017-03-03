2d932f9d8a04f3fdd5f9859e50f5a2c81ae33635
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2017-03-03 19:50:10 +0100

    Test completion

--- test/features/dba_general_test.rb
+++ test/features/dba_general_test.rb
@@ -13,17 +13,40 @@ class DbaGeneralTest < Capybara::Rails::TestCase
     page.must_have_content 'Handling hints'
   end
 
-  test "DB-Locks current" do
+  test "DB-Locks / current" do
     login_and_menu_call('DBA general', 'DB-Locks', 'menu_dba_show_locks')
     page.must_have_content 'List current locks of different types'
 
     page.click_button 'DML-locks complete'
     wait_for_ajax
     page.must_have_content 'DML Database locks (from GV$Lock)'
-    # save_and_open_screenshot  # cannot work at headless server
 
+    # click first row column "SID/SN" in grid
+    page.find(:xpath, "//div[contains(@class, 'slick-cell') and contains(@class, 'l0') and contains(@class, 'r0')]").first('a').click
+    wait_for_ajax
+    page.must_have_content 'Details for session SID='
+
+    # click first row column "Module" in grid
+    page.first(:xpath, "//div[contains(@class, 'slick-inner-cell') and contains(@row, '0') and contains(@column, 'col2')]").first('a').click
+    wait_for_ajax
+
+    # click first row column "Action" in grid
+    page.first(:xpath, "//div[contains(@class, 'slick-inner-cell') and contains(@row, '0') and contains(@column, 'col3')]").first('a').click
+    wait_for_ajax
+
+    page.click_button 'Blocking DML-Locks'
+    wait_for_ajax
+    page.must_have_content 'Blocking DML-Locks from gv$Lock'
+
+    page.click_button 'Blocking DDL-Locks'
+    wait_for_ajax
+    page.must_have_content 'Blocking DDL-Locks in Library Cache (from DBA_KGLLock)'
+
+    # save_and_open_screenshot  # cannot work at headless server
   end
-end
+
+  test "DB-Locks / Blocking locks historic" do
+  end
 
 
 
