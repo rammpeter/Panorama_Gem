f48bfcebe823f714cc849fe3c0887fef9b75678f
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2017-03-13 18:03:56 +0100

    Separate List for pending 2pc

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -1,7 +1,7 @@
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.5.26'
-  RELEASE_DATE = Date.parse('2017-02-28')
+  VERSION = '2.5.27'
+  RELEASE_DATE = Date.parse('2017-03-13')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
