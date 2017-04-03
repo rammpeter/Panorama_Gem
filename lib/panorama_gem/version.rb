bfa7a0c0cc5ce2789194687c96ea4a2b9973b165
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2017-04-03 12:46:25 +0200

    Remove erroneous function ajax_post_call

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -1,7 +1,7 @@
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.5.31'
-  RELEASE_DATE = Date.parse('2017-03-23')
+  VERSION = '2.5.32'
+  RELEASE_DATE = Date.parse('2017-04-03')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
