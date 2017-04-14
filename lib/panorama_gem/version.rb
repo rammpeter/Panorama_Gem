59c90aef36ae118eaa5cedf47d8c31a8a0629e8e
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2017-04-14 15:30:14 +0200

    Switch to jruby 9.1.8

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -1,7 +1,9 @@
+require 'date' 
+
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.5.33'
-  RELEASE_DATE = Date.parse('2017-04-12')
+  VERSION = '2.5.34'
+  RELEASE_DATE = Date.parse('2017-04-14')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
