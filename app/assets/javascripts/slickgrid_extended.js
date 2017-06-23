031f3746739e8e291d392a7ceba819b01fb1b83a
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2017-06-23 22:53:31 +0200

    Show minutes and hours in mouseover hint

--- app/assets/javascripts/slickgrid_extended.js
+++ app/assets/javascripts/slickgrid_extended.js
@@ -1459,7 +1459,7 @@ function HTMLFormatter(row, cell, value, columnDef, dataContext){
     }
     if (columnDef['show_pct_hint'] && columnDef['column_sum'] > 0 ){
         var pct_value = slickGrid.parseFloatLocale(value) * 100 / columnDef['column_sum'];
-        title += ". "+ slickGrid.printFloatLocale(pct_value, 2) + ' % ' + slickGrid.ext_locale_translate('slickgrid_pct_hint') + ' ' + slickGrid.printFloatLocale(columnDef['column_sum'], 0);
+        title += ".\n= "+ slickGrid.printFloatLocale(pct_value, 2) + ' % ' + slickGrid.ext_locale_translate('slickgrid_pct_hint') + ' ' + slickGrid.printFloatLocale(columnDef['column_sum'], 0);
     }
     if (title.length > 0){
         output += " title='"+title+"'";
