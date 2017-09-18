5db9f1227d59ed6d2d34a5836b4212db3cdda62f
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2017-09-18 17:48:25 +0200

    SQL translations for 12.1.0.1

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,8 +2,8 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.5.97'
-  RELEASE_DATE = Date.parse('2017-09-16')
+  VERSION = '2.5.98'
+  RELEASE_DATE = Date.parse('2017-09-18')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
