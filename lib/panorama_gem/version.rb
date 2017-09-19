c1e41dbc17515a5b1b02131464ddfdb24f31ba21
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2017-09-19 12:01:41 +0200

    Bugfix: Show SQL translation for > 12.1 only

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,7 +2,7 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.6.01'
+  VERSION = '2.6.02'
   RELEASE_DATE = Date.parse('2017-09-17')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
