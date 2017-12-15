11777e39bc61078b0d9fb4cc078746a730fbb2a3
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2017-12-15 20:34:43 +0100

    Fix diagram for ASH, some browswer_tab_id

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,7 +2,7 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.6.35'
+  VERSION = '2.6.36'
   RELEASE_DATE = Date.parse('2017-12-15')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
