91153b31a8d772c1a3b5f32cadc0a9fe670d5a84
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2017-12-15 13:30:34 +0100

    Retry if connection pool is full, size 100

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,8 +2,8 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.6.34'
-  RELEASE_DATE = Date.parse('2017-12-14')
+  VERSION = '2.6.35'
+  RELEASE_DATE = Date.parse('2017-12-15')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
