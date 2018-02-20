030199dc52848339b7d8edac7a1d5bab3924b95c
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2018-02-20 13:20:22 +0100

    Allow choice of diagnostics and tuning pack if DB <= 11.1

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,7 +2,7 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.7.11'
+  VERSION = '2.7.12'
   RELEASE_DATE = Date.parse('2017-02-20')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
