03481acd6ce84809d83df55aaa65ecbe39952aba
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2018-03-11 15:01:36 +0100

    DBA_Hist_IOStat_Filetype added in Panorama-Sampler

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,8 +2,8 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.7.16'
-  RELEASE_DATE = Date.parse('2017-03-08')
+  VERSION = '2.7.17'
+  RELEASE_DATE = Date.parse('2017-03-11')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
