18732f166420a6cf292c5766f39aa96250ea8f48
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2018-03-27 13:05:25 +0200

    Test without management_pack: dba_history

--- test/controllers/dba_history_controller_test.rb
+++ test/controllers/dba_history_controller_test.rb
@@ -9,22 +9,28 @@ class DbaHistoryControllerTest < ActionDispatch::IntegrationTest
 
     initialize_min_max_snap_id_and_times
 
-    @sga_sql_id_without_history = sql_select_one "SELECT SQL_ID
-                                                  FROM   v$SQLArea
-                                                  WHERE  SQL_ID NOT IN (SELECT SQL_ID FROM DBA_Hist_SQLText)
-                                                  AND    RowNum < 2"
-    # Find a SQL_ID that surely exists in History
-    sql_row = sql_select_first_row "SELECT MAX(SQL_ID)              KEEP (DENSE_RANK LAST ORDER BY Occurs) SQL_ID,
-                                           MAX(Parsing_Schema_Name) KEEP (DENSE_RANK LAST ORDER BY Occurs) Parsing_Schema_Name
-                                    FROM   (
-                                            SELECT SQL_ID, Parsing_Schema_Name, COUNT(*) Occurs
-                                            FROM   DBA_Hist_SQLStat s
-                                            WHERE  s.Snap_ID > (SELECT MAX(Snap_ID) FROM DBA_Hist_Snapshot) - 20
-                                            GROUP BY SQL_ID, Parsing_Schema_Name
-                                           )
-                                   "
-    @hist_sql_id = sql_row.sql_id
-    @hist_parsing_schema_name = sql_row.parsing_schema_name
+    if management_pack_license == :none                                         # Fake defaults if no management pack license
+      @sga_sql_id_without_history = '12345'
+      @hist_sql_id                = '12345'
+      @hist_parsing_schema_name   = 'HUGO'
+    else
+      @sga_sql_id_without_history = sql_select_one "SELECT SQL_ID
+                                                    FROM   v$SQLArea
+                                                    WHERE  SQL_ID NOT IN (SELECT SQL_ID FROM DBA_Hist_SQLText)
+                                                    AND    RowNum < 2"
+      # Find a SQL_ID that surely exists in History
+      sql_row = sql_select_first_row "SELECT MAX(SQL_ID)              KEEP (DENSE_RANK LAST ORDER BY Occurs) SQL_ID,
+                                             MAX(Parsing_Schema_Name) KEEP (DENSE_RANK LAST ORDER BY Occurs) Parsing_Schema_Name
+                                      FROM   (
+                                              SELECT SQL_ID, Parsing_Schema_Name, COUNT(*) Occurs
+                                              FROM   DBA_Hist_SQLStat s
+                                              WHERE  s.Snap_ID > (SELECT MAX(Snap_ID) FROM DBA_Hist_Snapshot) - 20
+                                              GROUP BY SQL_ID, Parsing_Schema_Name
+                                             )
+                                     "
+      @hist_sql_id = sql_row.sql_id
+      @hist_parsing_schema_name = sql_row.parsing_schema_name
+    end
   end
 
   # Alle Menu-Einträge testen für die der Controller eine Action definiert hat
@@ -35,16 +41,16 @@ class DbaHistoryControllerTest < ActionDispatch::IntegrationTest
 
   test "segment_stat_historic with xhr: true" do
     post '/dba_history/list_segment_stat_historic_sum', :params => {:format=>:html,  :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end, :update_area=>:hugo }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
     post '/dba_history/list_segment_stat_historic_sum', :params => {:format=>:html,  :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end, :instance=>1, :update_area=>:hugo }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
 
     post '/dba_history/list_segment_stat_hist_detail', :params => {:format=>:html, :instance=>1, :min_snap_id=>@min_snap_id, :max_snap_id=>@max_snap_id, :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end,
          :owner=>'sys', :object_name=>'SEG$', :update_area=>:hugo }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
 
     post '/dba_history/list_segment_stat_hist_sql', :params => {:format=>:html, :instance=>1,  :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end, :owner =>"sys", :object_name=> "all_tables", :update_area=>:hugo }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
   end
 
   test "sql_area_historic with xhr: true" do
@@ -70,7 +76,7 @@ class DbaHistoryControllerTest < ActionDispatch::IntegrationTest
                                                       :sql_id               => sql_id,
                                                       :instance             => instance,
                                                       :update_area          => :hugo }
-            assert_response :success
+            assert_response management_pack_license == :none ? :error : :success
           end
         end
       end
@@ -80,7 +86,7 @@ class DbaHistoryControllerTest < ActionDispatch::IntegrationTest
   test 'list_sql_historic_execution_plan with xhr: true' do
     post '/dba_history/list_sql_historic_execution_plan', :params => {:format=>:html, :sql_id=>@hist_sql_id, :instance=>1, :parsing_schema_name=>@hist_parsing_schema_name,
                                                         :min_snap_id=>@min_snap_id, :max_snap_id=>@max_snap_id, :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end, :update_area=>:hugo }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
   end
 
   test 'list_sql_history_snapshots with xhr: true' do
@@ -96,7 +102,7 @@ class DbaHistoryControllerTest < ActionDispatch::IntegrationTest
                                                           :time_selection_start => ts[:time_selection_start],
                                                           :time_selection_end   => ts[:time_selection_end],
                                                           :update_area          => :hugo }
-            assert_response :success
+            assert_response management_pack_license == :none ? :error : :success
           end
         end
       end
@@ -120,7 +126,7 @@ Rails.logger.info "####################### SQL-ID=#{sql_id} #{@hist_sql_id} #{@s
           if sql_id == @sga_sql_id_without_history
             assert_response :redirect
           else
-            assert_response :success
+            assert_response management_pack_license == :none ? :error : :success
           end
         end
 
@@ -133,36 +139,36 @@ Rails.logger.info "####################### SQL-ID=#{sql_id} #{@hist_sql_id} #{@s
   test "show_using_sqls_historic with xhr: true" do
     post '/dba_history/show_using_sqls_historic', :params => {:format=>:html,  :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end,
                                     :ObjectName => "WRH$_sysmetric_history", :update_area=>:hugo }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
   end
 
   test "list_system_events_historic with xhr: true" do
     post '/dba_history/list_system_events_historic', :params => {:format=>:html, :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end,
          :instance=>1, :update_area=>:hugo }
-     assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
   end
 
   test "list_system_events_historic_detail with xhr: true" do
     post '/dba_history/list_system_events_historic_detail', :params => {:format=>:html,  :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end,
          :instance=>1, :min_snap_id=>@min_snap_id, :max_snap_id=>@max_snap_id, :event_id=>1, :event_name=>"Hugo", :update_area=>:hugo }
-     assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
   end
 
   test "list_system_statistics_historic with xhr: true" do
     post '/dba_history/list_system_statistics_historic', :params => {:format=>:html,  :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end, :stat_class=> {:bit => 1}, :instance=>1, :sum=>1, :update_area=>:hugo }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
     post '/dba_history/list_system_statistics_historic', :params => {:format=>:html,  :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end, :stat_class=> {:bit => 1}, :instance=>1, :full=>1, :verdichtung=>{:tag =>"MI"}, :update_area=>:hugo }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
     post '/dba_history/list_system_statistics_historic', :params => {:format=>:html,  :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end, :stat_class=> {:bit => 1}, :instance=>1, :full=>1, :verdichtung=>{:tag =>"HH24"}, :update_area=>:hugo }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
     post '/dba_history/list_system_statistics_historic', :params => {:format=>:html,  :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end, :stat_class=> {:bit => 1}, :instance=>1, :full=>1, :verdichtung=>{:tag =>"DD"}, :update_area=>:hugo }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
   end
 
   test "list_system_statistics_historic_detail with xhr: true" do
     post '/dba_history/list_system_statistics_historic_detail', :params => {:format=>:html,  :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end, :instance=>1,
          :min_snap_id=>@min_snap_id, :max_snap_id=>@max_snap_id, :stat_id=>1, :stat_name=>"Hugo", :update_area=>:hugo }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
   end
 
   test "list_sysmetric_historic with xhr: true" do
@@ -181,22 +187,22 @@ Rails.logger.info "####################### SQL-ID=#{sql_id} #{@hist_sql_id} #{@s
      time_selection_start = time_selection_start.strftime("%d.%m.%Y %H:%M")
 
      post '/dba_history/list_sysmetric_historic', :params => {:format=>:html,  :time_selection_start =>time_selection_start, :time_selection_end =>time_selection_end, :detail=>1, :grouping=>{:tag =>grouping}, :update_area=>:hugo }
-     assert_response :success
+     assert_response management_pack_license == :none ? :error : :success
      post '/dba_history/list_sysmetric_historic', :params => {:format=>:html,  :time_selection_start =>time_selection_start, :time_selection_end =>time_selection_end, :instance=>1, :detail=>1, :grouping=>{:tag =>grouping}, :update_area=>:hugo }
-     assert_response :success
+     assert_response management_pack_license == :none ? :error : :success
      post '/dba_history/list_sysmetric_historic', :params => {:format=>:html,  :time_selection_start =>time_selection_start, :time_selection_end =>time_selection_end, :summary=>1, :grouping=>{:tag =>grouping}, :update_area=>:hugo }
-     assert_response :success
+     assert_response management_pack_license == :none ? :error : :success
      post '/dba_history/list_sysmetric_historic', :params => {:format=>:html,  :time_selection_start =>time_selection_start, :time_selection_end =>time_selection_end, :instance=>1, :summary=>1, :grouping=>{:tag =>grouping}, :update_area=>:hugo }
-     assert_response :success
+     assert_response management_pack_license == :none ? :error : :success
    end
   end
 
   test "mutex_statistics_historic with xhr: true" do
     [:Blocker, :Waiter, :Timeline].each do |submit_name|
       post '/dba_history/list_mutex_statistics_historic', :params => {:format=>:html, :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end, :instance=>1, submit_name=>"Hugo", :update_area=>:hugo }
-      assert_response :success
+      assert_response management_pack_license == :none ? :error : :success
       post '/dba_history/list_mutex_statistics_historic', :params => {:format=>:html, :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end, submit_name=>"Hugo", :update_area=>:hugo }
-      assert_response :success
+      assert_response management_pack_license == :none ? :error : :success
     end
 
     get '/dba_history/list_mutex_statistics_historic_samples', :params => {:format=>:html, :instance=>1, :mutex_type=>:Hugo, :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end,
@@ -210,60 +216,60 @@ Rails.logger.info "####################### SQL-ID=#{sql_id} #{@hist_sql_id} #{@s
 
   test "latch_statistics_historic with xhr: true" do
     post '/dba_history/list_latch_statistics_historic', :params => {:format=>:html, :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end, :instance=>1 }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
 
     post '/dba_history/list_latch_statistics_historic_details', :params => {:format=>:html, :instance=>1, :min_snap_id=>@min_snap_id, :max_snap_id=>@max_snap_id,
          :latch_hash => 12313123, :latch_name=>"Hugo" }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
   end
 
   test "enqueue_statistics_historic with xhr: true" do
     post '/dba_history/list_enqueue_statistics_historic', :params => {:format=>:html, :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_start, :instance=>1 }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
 
     post '/dba_history/list_enqueue_statistics_historic_details', :params => {:format=>:html, :instance=>1, :min_snap_id=>@min_snap_id, :max_snap_id=>@max_snap_id,
          :eventno => 12313123, :reason=>"Hugo", :description=>"Hugo" }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
   end
 
   test "list_compare_sql_area_historic with xhr: true" do
     tag1 = Time.new
     post '/dba_history/list_compare_sql_area_historic', :params => {:format=>:html, :instance=>1, :filter=>"Hugo", :sql_id=>@hist_sql_id, :minProzDiff=>50,
                                                       :tag1=> tag1.strftime("%d.%m.%Y"), :tag2=>(tag1-86400).strftime("%d.%m.%Y") }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
   end
 
   test "genuine_oracle_reports with xhr: true" do
     post '/dba_history/list_awr_report_html', :params => {:format=>:html, :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end, :instance=>1 }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
 
     post '/dba_history/list_awr_global_report_html', :params => {:format=>:html, :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
 
     post '/dba_history/list_awr_global_report_html', :params => {:format=>:html, :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end, :instance=>1 }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
 
     post '/dba_history/list_ash_report_html', :params => {:format=>:html, :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end, :instance=>1 }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
 
     post '/dba_history/list_ash_global_report_html', :params => {:format=>:html, :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
 
     post '/dba_history/list_ash_global_report_html', :params => {:format=>:html, :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end, :instance=>1 }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
 
     post '/dba_history/list_awr_sql_report_html', :params => {:format=>:html, :time_selection_start =>@time_selection_start, :time_selection_end =>@time_selection_end, :instance=>1, :sql_id=>@hist_sql_id }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
   end
 
   test "generate_baseline_creation with xhr: true" do
     post '/dba_history/generate_baseline_creation', :params => {:format=>:html, :sql_id=>@hist_sql_id, :min_snap_id=>@min_snap_id, :max_snap_id=>@max_snap_id, :plan_hash_value=>1234567, :update_area=>:hugo }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
   end
 
   test "select_plan_hash_value_for_baseline with xhr: true" do
     post '/dba_history/select_plan_hash_value_for_baseline', :params => {:format=>:html, :sql_id=>@hist_sql_id, :min_snap_id=>@min_snap_id, :max_snap_id=>@max_snap_id, :update_area=>:hugo }
-    assert_response :success
+    assert_response management_pack_license == :none ? :error : :success
 
   end
 
@@ -278,7 +284,7 @@ Rails.logger.info "####################### SQL-ID=#{sql_id} #{@hist_sql_id} #{@s
             resource:             {name: resname_rec.resource_name},
             update_area:          :hugo
         }
-        assert_response :success
+        assert_response management_pack_license == :none ? :error : :success
       end
     end
   end
@@ -289,7 +295,7 @@ Rails.logger.info "####################### SQL-ID=#{sql_id} #{@hist_sql_id} #{@s
         [{sql_id: @hist_sql_id}, {sid: 1, serialno: 2}].each do |p|
           post '/dba_history/list_sql_monitor_reports', params: {format: :html, instance: instance, sql_id: p[:sql_id], sid: p[:sid], serialno: p[:serialno],
                                                                  time_selection_start: @time_selection_start, time_selection_end: @time_selection_end, update_area: :hugo }
-          assert_response :success
+          assert_response management_pack_license == :none ? :error : :success
         end
       end
     end
@@ -310,7 +316,7 @@ Rails.logger.info "####################### SQL-ID=#{sql_id} #{@hist_sql_id} #{@s
                                                                        sql_exec_id:           1,
                                                                        origin:                origin,
                                                                        update_area: :hugo }
-        assert_response :success
+        assert_response management_pack_license == :none ? :error : :success
       end
     end
   end
