4774277e3e1b5f994752ccf43e808c35a3ca2144
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2018-04-09 10:48:36 +0200

    Debug JDBC driver

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,8 +2,8 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.7.24'
-  RELEASE_DATE = Date.parse('2017-03-23')
+  VERSION = '2.7.25'
+  RELEASE_DATE = Date.parse('2017-04-09')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
