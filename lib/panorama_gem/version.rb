bbcbd71d1405738c851131ac873b5da6fb969079
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2018-05-05 22:16:01 +0200

    Check maximum path length at startup

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,7 +2,7 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.7.35'
+  VERSION = '2.7.36'
   RELEASE_DATE = Date.parse('2017-05-05')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
