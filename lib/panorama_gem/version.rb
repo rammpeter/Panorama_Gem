574fc5cd655bba493bd3964d9a6dbcefe6db98fe
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2018-05-08 11:14:02 +0200

    Compress Indexes for Panorama-Sampler

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,8 +2,8 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.7.37'
-  RELEASE_DATE = Date.parse('2017-05-06')
+  VERSION = '2.7.38'
+  RELEASE_DATE = Date.parse('2017-05-08')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
