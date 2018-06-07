5aa857eaecfccab4e79d9a150e75b2ce48158fcd
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2018-06-07 12:59:03 +0200

    Tables without read

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,7 +2,7 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.7.50'
+  VERSION = '2.7.51'
   RELEASE_DATE = Date.parse('2017-06-07')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
