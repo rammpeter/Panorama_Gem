575ca246fb38202a0eed0c137e07d3f0a6c76b9d
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2018-06-20 21:40:33 +0200

    SQL worksheet established

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,8 +2,8 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.7.52'
-  RELEASE_DATE = Date.parse('2017-06-12')
+  VERSION = '2.7.53'
+  RELEASE_DATE = Date.parse('2017-06-20')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
