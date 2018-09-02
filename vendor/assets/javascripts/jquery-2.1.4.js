4552ab25791c2aef2d57bbcf4d941440cb13bfd8
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2018-09-02 17:39:21 +0200

    Switch to css for coreui-fonts

--- vendor/assets/javascripts/jquery-2.1.4.js
+++ vendor/assets/javascripts/jquery-2.1.4.js
@@ -4459,7 +4459,7 @@ jQuery.event = {
 			cur = event.target;
 
 		// Find delegate handlers
-		// Black-hole SVG <use> instance trees (#13180)
+		// Black-hole SVG-orig <use> instance trees (#13180)
 		// Avoid non-left-click bubbling in Firefox (#3861)
 		if ( delegateCount && cur.nodeType && (!event.button || event.type !== "click") ) {
 
