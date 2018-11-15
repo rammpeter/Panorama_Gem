a9a9b1d34f0478632b5bdaa5355365f22e10caf4
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2018-11-15 18:05:49 +0100

    exchange jQuery for jQuery(document).ready

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,7 +2,7 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.7.86'
+  VERSION = '2.7.87'
   RELEASE_DATE = Date.parse('2017-11-15')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
