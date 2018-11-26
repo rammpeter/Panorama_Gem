63cdd272704e29f7669045c7c842122538476482
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2018-11-26 12:42:17 +0100

    Fix bug in ASH-listing if more than one " in module name

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,8 +2,8 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.7.93'
-  RELEASE_DATE = Date.parse('2017-11-22')
+  VERSION = '2.7.94'
+  RELEASE_DATE = Date.parse('2017-11-26')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
