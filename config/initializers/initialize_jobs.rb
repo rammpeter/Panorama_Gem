605086d580f2bcfb549fec97b2230afcd5bc7991
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2019-01-17 10:26:30 +0100

    Restore behavior after entropy tests

--- config/initializers/initialize_jobs.rb
+++ config/initializers/initialize_jobs.rb
@@ -3,6 +3,6 @@
 require_relative '../../config/engine_config'
 
 # Wait async to proceed rails startup before first job execution
-# PanoramaSamplerJob.set(wait: 5.seconds).perform_later if !EngineConfig.config.panorama_sampler_master_password.nil?
+ PanoramaSamplerJob.set(wait: 5.seconds).perform_later if !EngineConfig.config.panorama_sampler_master_password.nil?
 
-# ConnectionTerminateJob.set(wait: 10.seconds).perform_later                      # Check connections for inactivity
+ ConnectionTerminateJob.set(wait: 10.seconds).perform_later                      # Check connections for inactivity
