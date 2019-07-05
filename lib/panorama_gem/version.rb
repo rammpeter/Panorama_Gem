56edf45a5a3fbd3b0b8932551eb3b973ac72286e
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2019-07-05 22:00:21 +0200

    Use my_html_escape instead of html_escape to translate line feeds to <br>

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,8 +2,8 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.8.91'
-  RELEASE_DATE = Date.parse('2019-07-04')
+  VERSION = '2.8.92'
+  RELEASE_DATE = Date.parse('2019-07-05')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
