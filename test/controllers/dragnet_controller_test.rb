1240ac6b1dab52e612dc55ecea369e7f3c352088
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2019-08-28 15:20:09 +0200

    Fix tests

--- test/controllers/dragnet_controller_test.rb
+++ test/controllers/dragnet_controller_test.rb
@@ -47,6 +47,7 @@ class DragnetControllerTest < ActionController::TestCase
 
           expected_result = :success                                            # May switch to error if license violation on DBA_Hist_xxx
           if !full_entry[:not_executable] && (full_entry[:min_db_version].nil? || full_entry[:min_db_version] <= get_db_version)
+            # Check if result should by error or success, Without management pack license execution should result in error if SQL contains DBA_HIST etc.
             begin
               prepare_panorama_sampler_thread_db_config                         # Ensure that PanoramaConnection has valid config even outside controller action
               PackLicense.filter_sql_for_pack_license(full_entry[:sql], management_pack_license)
