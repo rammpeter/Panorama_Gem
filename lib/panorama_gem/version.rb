de7d94574c46e446a382ce96abdbe38286b69eb1
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2019-12-11 15:00:10 +0100

    Switch to jRuby 9.2.9.0 and Rails 6.0.1

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,7 +2,7 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.9.28'
+  VERSION = '2.9.29'
   RELEASE_DATE = Date.parse('2019-12-11')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
