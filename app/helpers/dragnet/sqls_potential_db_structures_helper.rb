83af2f9567fb2081c85fd3cef3e5d54168950adf
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2019-12-13 11:02:26 +0100

    speedup dragnet SQL

--- app/helpers/dragnet/sqls_potential_db_structures_helper.rb
+++ app/helpers/dragnet/sqls_potential_db_structures_helper.rb
@@ -18,48 +18,59 @@ OLTP-compression requires licensing of Advanced Compression Option.
             '),
 
             :sql=> "\
-              SELECT Owner, Table_Name,
-                     SUM(CASE WHEN Partition_Name IS NULL THEN NULL ELSE 1 END) \"Partitions to Compress\",
-                     MAX(CASE WHEN Partition_Name IS NULL THEN NULL ELSE Partitions_Total END) \"Partitions Total\",
-                     SUM(Num_Rows) Num_Rows,
-                     SUM(Size_MB)  Size_MB,
-                     MAX(Last_Analyzed) Max_Last_analyzed,
-                     SUM(Inserts) Inserts,
-                     SUM(Updates) Updates,
-                     SUM(Deletes) Deletes,
-                     MAX(Last_DML) Last_DML
-              FROM   (
-                      SELECT x.*, ROUND(s.Bytes/(1024*1024),2) Size_MB
-                      FROM   (SELECT /*+ NO_MERGE */ x.*, COUNT(*) OVER (PARTITION BY Owner, Table_Name) Partitions_Total
-                              FROM   (
-                                      SELECT t.Owner, t.Table_Name, t.Num_Rows, t.Last_Analyzed, NULL Partition_Name, m.Inserts, m.Updates, m.Deletes, m.Timestamp Last_DML, t.Compression
-                                      FROM   DBA_Tables t
-                                      LEFT OUTER JOIN DBA_Tab_Modifications m ON m.Table_Owner = t.Owner AND m.Table_Name = t.Table_Name AND m.Partition_Name IS NULL
-                                      WHERE  t.Partitioned = 'NO'
-                                      UNION ALL
-                                      SELECT t.Table_Owner Owner, t.Table_Name, t.Num_Rows, t.Last_Analyzed, t.Partition_Name, m.Inserts, m.Updates, m.Deletes, m.Timestamp Last_DML, t.Compression
-                                      FROM   DBA_Tab_Partitions t
-                                      LEFT OUTER JOIN DBA_Tab_Modifications m ON m.Table_Owner = t.Table_Owner AND m.Table_Name = t.Table_Name AND m.Partition_Name = t.Partition_Name
-                                      WHERE  t.Composite = 'NO'
-                                      UNION ALL
-                                      SELECT t.Table_Owner Owner, t.Table_Name, t.Num_Rows, t.Last_Analyzed, t.SubPartition_Name Partition_Name, m.Inserts, m.Updates, m.Deletes, m.Timestamp Last_DML, t.Compression
-                                      FROM   DBA_Tab_SubPartitions t
-                                      LEFT OUTER JOIN DBA_Tab_Modifications m ON m.Table_Owner = t.Table_Owner AND m.Table_Name = t.Table_Name AND m.Partition_Name = t.Partition_Name AND m.SubPartition_Name = t.SubPartition_Name
-                                     ) x
-                             ) x
-                      LEFT OUTER JOIN DBA_Segments s ON s.Owner = x.Owner AND s.Segment_Name = x.Table_Name AND NVL(s.Partition_Name, '-1') = NVL(x.Partition_Name, '-1')
-                      WHERE  x.Owner NOT IN (#{system_schema_subselect})
-                      AND    x.Compression = 'DISABLED'
-                      AND    s.Bytes/(1024*1024) > ?
-                      AND    (   x.Updates IS NULL                              -- no DML since last analyze
-                              OR x.Updates < (x.Inserts + x.Deletes)/(100/?)    -- updates less than limit
-                             )
-                    )
-              GROUP BY Owner, Table_Name
-              ORDER BY Size_MB DESC
+WITH Segments AS (SELECT /*+ NO_MERGE MATERIALIZE */ Owner, Segment_Name, Partition_Name, ROUND(Bytes/(1024*1024),2) Size_MB
+                  FROM   DBA_Segments
+                  WHERE  Owner NOT IN (#{system_schema_subselect})
+                  AND    Bytes/(1024*1024) > ?
+                 ),
+     Tables AS   (SELECT /*+ NO_MERGE MATERIALIZE */ t.Owner, t.Table_Name, t.Num_Rows, t.Last_Analyzed, NULL Partition_Name,
+                         m.Inserts, m.Updates, m.Deletes, m.Timestamp Last_DML, t.Compression
+                  FROM   DBA_Tables t
+                  LEFT OUTER JOIN DBA_Tab_Modifications m ON m.Table_Owner = t.Owner AND m.Table_Name = t.Table_Name AND m.Partition_Name IS NULL
+                  AND    t.Compression = 'DISABLED'
+                 ),
+     Partitions AS (SELECT /*+ NO_MERGE MATERIALIZE */ t.Table_Owner Owner, t.Table_Name, t.Num_Rows, t.Last_Analyzed, t.Partition_Name,
+                           m.Inserts, m.Updates, m.Deletes, m.Timestamp Last_DML, t.Compression
+                    FROM   DBA_Tab_Partitions t
+                    LEFT OUTER JOIN DBA_Tab_Modifications m ON m.Table_Owner = t.Table_Owner AND m.Table_Name = t.Table_Name AND m.Partition_Name = t.Partition_Name
+                    WHERE  t.Composite = 'NO'
+                   ),
+     SubPartitions AS (SELECT /*+ NO_MERGE MATERIALIZE */ t.Table_Owner Owner, t.Table_Name, t.Num_Rows, t.Last_Analyzed, t.SubPartition_Name Partition_Name,
+                              m.Inserts, m.Updates, m.Deletes, m.Timestamp Last_DML, t.Compression
+                       FROM   DBA_Tab_SubPartitions t
+                       LEFT OUTER JOIN DBA_Tab_Modifications m ON m.Table_Owner = t.Table_Owner AND m.Table_Name = t.Table_Name AND m.Partition_Name = t.Partition_Name AND m.SubPartition_Name = t.SubPartition_Name
+                      ),
+     Objects AS  (SELECT /*+ NO_MERGE MATERIALIZE */ x.*, COUNT(*) OVER (PARTITION BY Owner, Table_Name) Partitions_Total
+                  FROM   (
+                          SELECT * FROM Tables t
+                          UNION ALL
+                          SELECT * FROM Partitions
+                          UNION ALL
+                          SELECT * FROM SubPartitions
+                         ) x
+                  WHERE  x.Owner NOT IN (#{system_schema_subselect})
+                  AND    x.Compression = 'DISABLED'
+                  AND    (   x.Updates IS NULL                              -- no DML since last analyze
+                          OR x.Updates < (x.Inserts + x.Deletes)/(100/?)    -- updates less than limit
+                         )
+                 )
+SELECT x.Owner, x.Table_Name,
+       SUM(CASE WHEN x.Partition_Name IS NULL THEN NULL ELSE 1 END) \"Partitions to Compress\",
+       MAX(CASE WHEN x.Partition_Name IS NULL THEN NULL ELSE Partitions_Total END) \"Partitions Total\",
+       SUM(Num_Rows) Num_Rows,
+       SUM(Size_MB)  Size_MB,
+       MAX(Last_Analyzed) Max_Last_analyzed,
+       SUM(Inserts) Inserts,
+       SUM(Updates) Updates,
+       SUM(Deletes) Deletes,
+       MAX(Last_DML) Last_DML
+FROM   Objects x
+JOIN Segments s ON s.Owner = x.Owner AND s.Segment_Name = x.Table_Name AND NVL(s.Partition_Name, '-1') = NVL(x.Partition_Name, '-1')
+GROUP BY x.Owner, x.Table_Name
+ORDER BY Size_MB DESC NULLS LAST
             ",
             :parameter=>[
-                {:name=>t(:dragnet_helper_50_param_1_name, :default=> 'Minimum size of table in MB'), :size=>8, :default=>100, :title=>t(:dragnet_helper_50_param_1_hint, :default=> 'Minimum size of table in MB for consideration in result of selection') },
+                {:name=>t(:dragnet_helper_50_param_1_name, :default=> 'Minimum size of table or partition in MB'), :size=>8, :default=>10, :title=>t(:dragnet_helper_50_param_1_hint, :default=> 'Minimum size of table, partition or subpartition in MB for consideration in result of selection') },
                 {:name=>t(:dragnet_helper_50_param_2_name, :default=> 'Maximum % of udates compared to inserts + deletes'), :size=>8, :default=>5, :title=>t(:dragnet_helper_50_param_2_hint, :default=> 'Maximum percentage of udate operations sind last analyze compared to the number of inserts + deletes') },
             ]
         },
