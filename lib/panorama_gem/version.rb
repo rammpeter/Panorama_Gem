2a5ed34258fecffcc208ae86b845acfe9d71a023
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2020-05-25 12:31:14 +0200

    move to Rails 6.0.3.1 and jruby 9.2.11.1 with version

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,8 +2,8 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.9.41'
-  RELEASE_DATE = Date.parse('2020-05-19')
+  VERSION = '2.9.42'
+  RELEASE_DATE = Date.parse('2020-05-25')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
