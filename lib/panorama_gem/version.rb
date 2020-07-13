3f647d1637d7b86fa3ccf42d489ddbc7f5a4c1d9
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2020-07-13 12:33:27 +0200

    include map files in application.scss

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,8 +2,8 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.9.48'
-  RELEASE_DATE = Date.parse('2020-07-08')
+  VERSION = '2.9.49'
+  RELEASE_DATE = Date.parse('2020-07-13')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
