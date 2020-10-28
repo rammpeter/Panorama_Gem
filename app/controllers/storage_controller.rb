a11f572ff0927bcd4ae2d65ccccdd5b9d311a853
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2020-10-28 22:11:51 +0100

    Show fill level of FRA

--- app/controllers/storage_controller.rb
+++ app/controllers/storage_controller.rb
@@ -140,8 +140,10 @@ class StorageController < ApplicationController
       totals['Fast Recovery Area'] = {'content_hint' => 'Fast Recovery Area'}
       totals['Fast Recovery Area']['mbtotal'] = @fra_size_bytes / (1024*1024).to_i
       totals['Fast Recovery Area']['mbused'] = 0
+      @fra_not_reclaimable_usage = 0
       @fra_usage.each do |f|
         totals['Fast Recovery Area']['mbused'] += f.percent_space_used*@fra_size_bytes/(1024*1024)/100
+        @fra_not_reclaimable_usage += f.percent_space_used - f.percent_space_reclaimable
       end
       totals['Fast Recovery Area']['mbfree'] = totals['Fast Recovery Area']['mbtotal'] - totals['Fast Recovery Area']['mbused']
 
--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,8 +2,8 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.9.71'
-  RELEASE_DATE = Date.parse('2020-10-06')
+  VERSION = '2.9.72'
+  RELEASE_DATE = Date.parse('2020-10-28')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
