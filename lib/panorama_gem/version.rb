48f80648ff72152e24afe6f33b69d4762ff03016
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2020-11-12 11:47:13 +0100

    Refreshed gemfile.lock with current versions of dependencies

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,8 +2,8 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.9.76'
-  RELEASE_DATE = Date.parse('2020-11-11')
+  VERSION = '2.9.77'
+  RELEASE_DATE = Date.parse('2020-11-12')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
