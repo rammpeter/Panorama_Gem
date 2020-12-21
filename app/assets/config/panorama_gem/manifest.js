072172689565a21d39ea9f82e50fa0e571071469
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2020-12-21 18:56:43 +0100

    sprockets manifest.js in gem-path

---
+++ app/assets/config/panorama_gem/manifest.js
@@ -0,0 +1,4 @@
+// File required by sprockets-rails v3.2.2 lib/sprockets/railtie.rb:106
+//= link_tree ../../images
+//= link_directory ../../stylesheets .css
+//= link_directory ../../javascripts .js
--- test/dummy/app/assets/config/manifest.js
+++ test/dummy/app/assets/config/manifest.js
@@ -1,4 +1,2 @@
 // File required by sprockets-rails v3.2.2 lib/sprockets/railtie.rb:106
-//= link_tree ../../../../../app/assets/images
-//= link_directory ../../../../../app/assets/stylesheets .css
-//= link_directory ../../../../../app/assets/javascripts .js
+//= link panorama_gem/manifest.js
