a5c0bd8c7fb94178787e6e6b0f2b6f3e421c838e
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2021-01-13 14:03:15 +0100

    Show fraction for blocking wait time, use blocking_sid as criteria for blocking records

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,8 +2,8 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.10.6'
-  RELEASE_DATE = Date.parse('2021-01-07')
+  VERSION = '2.10.7'
+  RELEASE_DATE = Date.parse('2021-01-13')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
