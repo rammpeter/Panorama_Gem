311ac832980dc9582386fdc55a9ed3596f158690
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2021-03-10 13:24:12 +0100

    format error message for sampler

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,8 +2,8 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.10.27'
-  RELEASE_DATE = Date.parse('2021-03-05')
+  VERSION = '2.10.28'
+  RELEASE_DATE = Date.parse('2021-03-10')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
