b10a0ea82149f012a9d1eba7d218665f782593f5
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2021-12-16 15:08:07 +0100

    Dragnet selection expanded for analyze dates

--- app/helpers/dragnet/instance_setup_tuning.rb
+++ app/helpers/dragnet/instance_setup_tuning.rb
@@ -32,7 +32,7 @@ Solution: recompile affected dependent objects
         },
         {
           :name  => t(:dragnet_helper_104_name, :default=>'Objekt statistics: Check on up-to-date analyze info (Tables)'),
-          :desc  => t(:dragnet_helper_104_desc, :default=>'For cost based optimizer object statistics should be sufficient up-to-date'),
+          :desc  => t(:dragnet_helper_104_desc, :default=>'Sufficient up-to-date object statistics should exist for cost-based optimizers'),
           :sql=> "\
 WITH Segments AS (SELECT /*+ NO_MERGE MATERIALIZE */ Owner, Segment_Name, Partition_Name, ROUND(SUM(Bytes/(1024*1024)),2) Size_MB
                   FROM   DBA_Segments
@@ -44,9 +44,13 @@ WITH Segments AS (SELECT /*+ NO_MERGE MATERIALIZE */ Owner, Segment_Name, Partit
                            FROM   DBA_Tab_Modifications
                            GROUP BY Table_Owner, Table_Name, Partition_Name, SubPartition_Name
                           )
-SELECT x.*
-FROM   (
-        SELECT t.Owner, t.Table_Name, NULL Partition_Name, NULL SubPartition_Name, t.Num_Rows, t.Last_Analyzed,
+SELECT CASE
+         WHEN Last_Analyzed IS NULL                       THEN 'Object never analyzed'
+         WHEN Last_Analyzed < SYSDATE-d.Min_Age           THEN 'Last analyzed too old'
+         WHEN x.Num_Rows = 0 AND x.MBytes > d.Min_MBytes  THEN 'Size does not match with rows=0'
+       END Possible_Problem,
+       x.*
+FROM   (SELECT t.Owner, t.Table_Name, NULL Partition_Name, NULL SubPartition_Name, t.Num_Rows, t.Last_Analyzed,
                s.MBytes, m.Inserts \"Inserts since last analyze\", m.Updates \"Updates since last analyze\", m.Deletes \"Deletes since last analyze\"
         FROM   DBA_Tables t
         LEFT OUTER JOIN (SELECT /*+ NO_MERGE */ Owner, Segment_Name, SUM(Size_MB) MBytes
@@ -74,50 +78,70 @@ FROM   (
         LEFT OUTER JOIN Segments s ON s.Owner = t.Table_Owner AND s.Segment_Name = t.Table_Name AND s.Partition_Name = t.SubPartition_Name
         LEFT OUTER JOIN Tab_Modifications m ON m.Table_Owner = t.Table_Owner AND m.Table_Name = t.Table_Name AND m.Partition_Name = t.Partition_Name AND m.SubPartition_Name = t.SubPartition_Name
        ) x
+CROSS JOIN (SELECT ? Min_Age, ? Min_Mbytes FROM DUAL) d
 WHERE  x.Owner NOT IN (#{system_schema_subselect})
-AND   (Last_Analyzed IS NULL OR Last_Analyzed < SYSDATE-?)
+AND   (   Last_Analyzed IS NULL
+       OR Last_Analyzed < SYSDATE-d.Min_Age
+       OR (x.Num_Rows = 0 AND x.MBytes > d.Min_MBytes)
+      )
 ORDER BY x.MBytes DESC NULLS LAST
                     ",
-          :parameter=>[{:name=>t(:dragnet_helper_104_param_1_name, :default=>'Minimum age of existing analyze info in days'), :size=>8, :default=>100, :title=>t(:dragnet_helper_104_param_1_hint, :default=>'If analyze info exists: minimun age for consideration in selection')}]
+          :parameter=>[
+            {:name=>t(:dragnet_helper_104_param_1_name, :default=>'Minimum age of existing analyze info in days'), :size=>8, :default=>1000, :title=>t(:dragnet_helper_104_param_1_hint, :default=>'If analyze info exists: minimun age for consideration in selection')},
+            {:name=>t(:dragnet_helper_104_param_2_name, :default=>'Minimum size (MB) if Num_Rows = 0'), :size=>8, :default=>100, :title=>t(:dragnet_helper_104_param_2_hint, :default=>'Minimum size of object in MB for check if num_rows=0 matches with size of object')}
+          ]
         },
         {
           :name  => t(:dragnet_helper_108_name, :default=>'Objekt statistics: Check on up-to-date analyze info (Indexes)'),
-          :desc  => t(:dragnet_helper_108_desc, :default=>'For cost based optimizer object statistics should be sufficient up-to-date'),
-          :sql=> "SELECT x.*
-                    FROM   (
-                            SELECT i.Owner, i.Table_Name, i.Index_Name, NULL Partition_Name, NULL SubPartition_Name, i.Num_Rows, i.Last_Analyzed,
-                                   ROUND(s.MBytes,2) MBytes
-                            FROM   DBA_Indexes i
-                            JOIN   DBA_Tables t ON t.Owner = i.Table_Owner AND t.Table_Name = i.Table_Name
-                            LEFT OUTER JOIN (SELECT /*+ NO_MERGE */ Owner, Segment_Name, SUM(Bytes)/(1024*1024) MBytes
-                                             FROM   DBA_Segments
-                                             GROUP BY Owner, Segment_Name
-                                            ) s ON s.Owner = i.Owner AND s.Segment_Name = i.Index_Name
-                            WHERE  t.Temporary = 'N'
-                            UNION ALL
-                            SELECT ip.Index_Owner Owner, i.Table_Name, ip.Index_Name, ip.Partition_Name, NULL SubPartition_Name, ip.Num_Rows, ip.Last_Analyzed,
-                                   ROUND(s.MBytes,2) MBytes
-                            FROM   DBA_Ind_Partitions ip
-                            JOIN   DBA_Indexes i ON i.Owner = ip.Index_Owner AND i.Index_Name = ip.Index_Name
-                            LEFT OUTER JOIN (SELECT /*+ NO_MERGE */ Owner, Segment_Name, Partition_Name, SUM(Bytes)/(1024*1024) MBytes
-                                             FROM   DBA_Segments
-                                             GROUP BY Owner, Segment_Name, Partition_Name
-                                            ) s ON s.Owner = ip.Index_Owner AND s.Segment_Name = ip.Index_Name AND s.Partition_Name = ip.Partition_Name
-                            UNION ALL
-                            SELECT ip.Index_Owner Owner, i.Table_Name, ip.Index_Name, ip.Partition_Name, ip.SubPartition_Name, ip.Num_Rows, ip.Last_Analyzed,
-                                   ROUND(s.MBytes,2) MBytes
-                            FROM   DBA_Ind_SubPartitions ip
-                            JOIN   DBA_Indexes i ON i.Owner = ip.Index_Owner AND i.Index_Name = ip.Index_Name
-                            LEFT OUTER JOIN (SELECT /*+ NO_MERGE */ Owner, Segment_Name, Partition_Name, SUM(Bytes)/(1024*1024) MBytes
-                                             FROM   DBA_Segments
-                                             GROUP BY Owner, Segment_Name, Partition_Name
-                                            ) s ON s.Owner = ip.Index_Owner AND s.Segment_Name = ip.Index_Name AND s.Partition_Name = ip.SubPartition_Name
-                           ) x
-                    WHERE  x.Owner NOT IN (#{system_schema_subselect})
-                    AND   (Last_Analyzed IS NULL OR Last_Analyzed < SYSDATE-?)
-                    ORDER BY x.MBytes DESC NULLS LAST
+          :desc  => t(:dragnet_helper_108_desc, :default=>'Sufficient up-to-date object statistics should exist for cost-based optimizers'),
+          :sql=> "\
+SELECT CASE
+         WHEN Last_Analyzed IS NULL                       THEN 'Object never analyzed'
+         WHEN Last_Analyzed < SYSDATE-d.Min_Age           THEN 'Last analyzed too old'
+         WHEN x.Num_Rows = 0 AND x.MBytes > d.Min_MBytes  THEN 'Size does not match with rows=0'
+       END Possible_Problem,
+       x.*
+FROM   (
+        SELECT i.Owner, i.Table_Name, i.Index_Name, NULL Partition_Name, NULL SubPartition_Name, i.Num_Rows, i.Last_Analyzed,
+               ROUND(s.MBytes,2) MBytes
+        FROM   DBA_Indexes i
+        JOIN   DBA_Tables t ON t.Owner = i.Table_Owner AND t.Table_Name = i.Table_Name
+        LEFT OUTER JOIN (SELECT /*+ NO_MERGE */ Owner, Segment_Name, SUM(Bytes)/(1024*1024) MBytes
+                         FROM   DBA_Segments
+                         GROUP BY Owner, Segment_Name
+                        ) s ON s.Owner = i.Owner AND s.Segment_Name = i.Index_Name
+        WHERE  t.Temporary = 'N'
+        UNION ALL
+        SELECT ip.Index_Owner Owner, i.Table_Name, ip.Index_Name, ip.Partition_Name, NULL SubPartition_Name, ip.Num_Rows, ip.Last_Analyzed,
+               ROUND(s.MBytes,2) MBytes
+        FROM   DBA_Ind_Partitions ip
+        JOIN   DBA_Indexes i ON i.Owner = ip.Index_Owner AND i.Index_Name = ip.Index_Name
+        LEFT OUTER JOIN (SELECT /*+ NO_MERGE */ Owner, Segment_Name, Partition_Name, SUM(Bytes)/(1024*1024) MBytes
+                         FROM   DBA_Segments
+                         GROUP BY Owner, Segment_Name, Partition_Name
+                        ) s ON s.Owner = ip.Index_Owner AND s.Segment_Name = ip.Index_Name AND s.Partition_Name = ip.Partition_Name
+        UNION ALL
+        SELECT ip.Index_Owner Owner, i.Table_Name, ip.Index_Name, ip.Partition_Name, ip.SubPartition_Name, ip.Num_Rows, ip.Last_Analyzed,
+               ROUND(s.MBytes,2) MBytes
+        FROM   DBA_Ind_SubPartitions ip
+        JOIN   DBA_Indexes i ON i.Owner = ip.Index_Owner AND i.Index_Name = ip.Index_Name
+        LEFT OUTER JOIN (SELECT /*+ NO_MERGE */ Owner, Segment_Name, Partition_Name, SUM(Bytes)/(1024*1024) MBytes
+                         FROM   DBA_Segments
+                         GROUP BY Owner, Segment_Name, Partition_Name
+                        ) s ON s.Owner = ip.Index_Owner AND s.Segment_Name = ip.Index_Name AND s.Partition_Name = ip.SubPartition_Name
+       ) x
+CROSS JOIN (SELECT ? Min_Age, ? Min_Mbytes FROM DUAL) d
+WHERE  x.Owner NOT IN (#{system_schema_subselect})
+AND   (   Last_Analyzed IS NULL
+       OR Last_Analyzed < SYSDATE-d.Min_Age
+       OR (x.Num_Rows = 0 AND x.MBytes > d.Min_MBytes)
+      )
+ORDER BY x.MBytes DESC NULLS LAST
                     ",
-          :parameter=>[{:name=>t(:dragnet_helper_108_param_1_name, :default=>'Minimum age of existing analyze info in days'), :size=>8, :default=>100, :title=>t(:dragnet_helper_108_param_1_hint, :default=>'If analyze info exists: minimun age for consideration in selection')}]
+          :parameter=>[
+            {:name=>t(:dragnet_helper_108_param_1_name, :default=>'Minimum age of existing analyze info in days'), :size=>8, :default=>1000, :title=>t(:dragnet_helper_108_param_1_hint, :default=>'If analyze info exists: minimun age for consideration in selection')},
+            {:name=>t(:dragnet_helper_104_param_2_name, :default=>'Minimum size (MB) if Num_Rows = 0'), :size=>8, :default=>100, :title=>t(:dragnet_helper_104_param_2_hint, :default=>'Minimum size of object in MB for check if num_rows=0 matches with size of object')}
+          ]
         },
 
     ]
