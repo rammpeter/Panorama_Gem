ca09468ab04b4702f833940074e3f15cfa58490c
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2022-01-11 17:53:51 +0100

    New dragnet selection for AUTO LIST partitioning

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,8 +2,8 @@ require 'date'
 
 module PanoramaGem
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.11.08'
-  RELEASE_DATE = Date.parse('2021-12-30')
+  VERSION = '2.12.0'
+  RELEASE_DATE = Date.parse('2022-01-11')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
