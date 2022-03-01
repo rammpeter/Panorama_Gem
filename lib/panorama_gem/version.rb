7b48b30a6ee4ed8534757c34ba4ba3e95aebc23b
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2022-03-01 09:08:06 +0100

    Update nokogiri

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,8 +2,8 @@ require 'date'
 
 module PanoramaGem
   # VERSION and REL EASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.13.11'
-  RELEASE_DATE = Date.parse('2022-02-21')
+  VERSION = '2.13.12'
+  RELEASE_DATE = Date.parse('2022-03-01')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
