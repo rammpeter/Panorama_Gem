b3aae69d0553d3e90a5cfe5338a83ac9dbfa87b7
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2022-03-02 17:20:40 +0100

    trigger CI action

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -4,7 +4,6 @@ module PanoramaGem
   # VERSION and REL EASE_DATE should have fix syntax and positions because they are parsed from other sites
   VERSION = '2.13.13'
   RELEASE_DATE = Date.parse('2022-03-02')
-
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
   RELEASE_YEAR  = "%04d" % RELEASE_DATE.year
