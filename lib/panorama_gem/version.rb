a458212011dec508bc73ddc66bb78bd2565f8dff
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2022-03-28 09:26:35 +0200

    Update jRuby to 9.3.4.0 and Rails to 6.1.5 Increase playwright timeout

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,8 +2,8 @@ require 'date'
 
 module PanoramaGem
   # VERSION and REL EASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.13.15'
-  RELEASE_DATE = Date.parse('2022-03-12')
+  VERSION = '2.13.16'
+  RELEASE_DATE = Date.parse('2022-03-28')
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
   RELEASE_YEAR  = "%04d" % RELEASE_DATE.year
