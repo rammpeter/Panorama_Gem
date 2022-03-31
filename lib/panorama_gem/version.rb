78ddd268b109b9657a789f0468ff5c259cc2dfbf
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2022-03-31 09:18:24 +0200

    Update puma due to CVE

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,7 +2,7 @@ require 'date'
 
 module PanoramaGem
   # VERSION and REL EASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.13.16'
+  VERSION = '2.13.17'
   RELEASE_DATE = Date.parse('2022-03-28')
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
