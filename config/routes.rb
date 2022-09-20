b68cec5a8b9963453bd874e26653777fd6870906
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2022-09-20 21:10:10 +0200

    Remove property padding-bottom:50px from class CodeMirror-scroll because this overlaps the areas above in Chrome

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
 
   # define routes as controller/action
   # Rails.logger.info "Panorama_Gem/config/routes.rb: Setting routes for every controller action"
+  sleep_count = 0
+  while EnvController.routing_actions("#{__dir__}/../app/controllers").count == 0 && sleep_count < 10 do
+    puts "Rails.application.routes.draw: EnvController.routing_actions is still empty! Retrying..."
+    sleep 1
+    sleep_count +=1
+  end
   EnvController.routing_actions("#{__dir__}/../app/controllers").each do |r|
     # puts "set route for #{r[:controller]}/#{r[:action]}"
     get  "#{r[:controller]}/#{r[:action]}"
