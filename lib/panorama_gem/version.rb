a508c308230d2d8fd8516390a723acba2e7f1b74
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2022-11-07 17:52:27 +0100

    Bundler updated

--- lib/panorama_gem/version.rb
+++ lib/panorama_gem/version.rb
@@ -2,7 +2,7 @@ require 'date'
 
 module PanoramaGem
   # VERSION and REL EASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.15.45'
+  VERSION = '2.15.46'
   RELEASE_DATE = Date.parse('2022-11-07')
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
